d50c6db2b15cc8273b03875eed456a7d9c70f957
Olfa <olfa_koubaa@yahoo.fr>
2013-10-09 13:59:27 +0000

    j-1

---
+++ TpObjet_loft/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 Loft l=new Loft(4,4);
+		 l.affiche();
+		 
+			 
+	}
+
+}
